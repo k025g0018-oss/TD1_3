65cac1be3a1a78f46485c7956380cc249658b885
ヤマト <k025g0093@g.neec.as.jp>
2026-01-08 15:59:36 +0900

    まだ少し難ありなプレイヤー

--- main.cpp
+++ main.cpp
@@ -114,7 +114,8 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		Novice::ScreenPrintf(0, 50, "pikachyuuuuu");
 		//
 		Novice::ScreenPrintf(0, 50, "pikachyuuuuu");
-
+		
+	
 
 
 		///
