7a259749e424249cafc69d334a514b457d0e6142
田谷 治幸 <nao.nao.y07@gmail.com>
2026-01-08 10:16:29 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <Novice.h>
+#define _USE_MATH_DEFINES
+#include <assert.h>
+#include <math.h>
+
+const char kWindowTitle[] = "TD1_3";
+
+//////////
+/// 構造体
+//////////
+
+struct Vector2 {
+	float x;
+	float y;
+};
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	//////////
+	/// 変数の宣言
+	//////////
+
+	// 画面
+	Vector2 screenSize;
+	screenSize.x = 1280.0f;
+	screenSize.y = 720.0f;
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, static_cast<int>(screenSize.x), static_cast<int>(screenSize.y));
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		//////////
+		/// 自機
+		//////////
+
+		//////////
+		/// 座標変換
+		//////////
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		//////////
+		/// 自機
+		//////////
+
+		//////////
+		/// デバッグ処理
+		//////////
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
