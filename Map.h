600539d6460db1eef16f85d292495eb1eee55243
AokiRen0074 <k025g0074@g.neec.ac.jp>
2026-01-08 12:43:49 +0900

    LDｔｋできるかもね

---
+++ Map.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Const.h"
+
+class Map {
+public:
+	// マップデータ（外部から参照できるようにpublicにする）
+	int mapData[kMapHeight][kMapWidth];
+
+	// コンストラクタ
+	Map();
+
+	// LDtk読み込み
+	void LoadMapFromLDtk(const char* fileName);
+
+	// 描画
+	void Draw(int textureHandle);
+};
---
+++ const.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+// マップ設定
+const int kMapHeight = 28;
+const int kMapWidth = 38;
+const int kTileSize = 32;
+
