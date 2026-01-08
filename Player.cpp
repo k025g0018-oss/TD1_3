4fda43deef0cb1fc704df8ec43c23cd227290c70
田谷 治幸 <nao.nao.y07@gmail.com>
2026-01-08 10:23:30 +0900

    (^▽^)/

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,62 @@
-#include "Player.h"
+﻿#include "Player.h"
+#include "Novice.h"
+
+Player::Player() {
+	InitPlayer();
+}
+
+void Player::InitPlayer() {
+	status_.pos.x = 300.0f;
+	status_.pos.y = 300.0f;
+	status_.acceleration.x = 0.0f;
+	status_.acceleration.y = 0.0f;
+	status_.Velocity.x = 0.0f;
+	status_.Velocity.y = +9.8f;
+	status_.Speed = 5.0f;
+	status_.scale.x = 64.0f;
+	status_.scale.y = 64.0f;
+	status_.isActive = true;
+	status_.isJumop = false;
+	status_.jumpPower=10.0f;
+}
+
+void Player::UpdatePlayer(char keys[256], char preKeys[256]){
+
+	if (status_.isActive) {
+		MovePlayer(keys, preKeys);
+	}
+}
+
+void Player::DrawPlayer() {
+	Novice::DrawBox(static_cast<int>(status_.pos.x), static_cast<int>(status_.pos.y),
+		static_cast<int>(status_.scale.x), static_cast<int>(status_.scale.y),
+		0.0f, WHITE, kFillModeSolid);
+}
+
+//------------------------------------------------------------------------------------------------------
+//プライベート関数など
+//------------------------------------------------------------------------------------------------------
+
+
+void Player::MovePlayer(char keys[256], char preKeys[256]) {
+
+	if (!status_.isJumop) {
+		if (keys[DIK_SPACE] && preKeys[DIK_SPACE]) {
+			status_.isJumop = true;
+			status_.Velocity.y =-status_.jumpPower;
+		}
+	}
+
+	if (status_.pos.y >= 1080) {
+		status_.pos.y = 0;
+	}
+	Gravity();
+}
+
+void Player::Gravity() {
+
+	status_.Velocity.y += 0.38f;
+	status_.pos.y += status_.Velocity.y;
+}
+
+
--- Player.h
+++ Player.h
@@ -1,4 +1,39 @@
-#pragma once
-class Player {
+﻿#pragma once
+#include "Vector2.h"
+
+class Player 
+{
+
+public:
+    // --- プレイヤーステータス ---
+    struct PlayerStatus
+    {
+        //プレイヤーの位置
+        Vector2 pos;
+
+        //プレイヤーの加速度
+        Vector2 acceleration;
+        Vector2 Velocity;//重力系
+        float Speed;
+
+        Vector2 scale;//大きさ
+        Vector2 dir;//プレイヤーの向き
+        
+        bool isActive;//生存フラグ
+        float jumpPower;//ジャンプ力
+        bool isJumop;//ジャンプフラグ
+    }status_;//status＿で宣言
+
+    Player();
+    void InitPlayer();
+    void UpdatePlayer(char keys[256], char preKeys[256]);
+    void DrawPlayer();
+
+private:
+
+    void MovePlayer(char keys[256], char preKeys[256]);
+    void Gravity();
+
+
 };
 
